7ace6c467003c848aec9564748512f90f6e083eb
IvanS <ismagin@yandex.ru>
2017-11-16 16:25:04 +0300

    working on  multilanguage support

--- lang/en.php
+++ lang/en.php
@@ -10,4 +10,9 @@
 $strings['unconfigured']="Before use you should adjust settings in config.php!";
 
 //Template
-$strings['disclaimer']='*Symlinks used to create library. This software is distributed "as is", <br>and author is not responsible for any problems you might get using it  <font color="red">including data loss or corruption!</font>*<br >';
+$strings['disclaimer']='*Symlinks used to create library. This software is distributed "as is", <br>and author is not responsible for any problems you might get using it, <font color="red">including data loss or corruption!</font>*<br >';
+$strings['path_to_root_media']="Path to root folder of source media files: ";
+$strings['path_to_root_library']="Path to your Plex anime library: ";
+$strings['main_page']="Main page";
+$strings['view_library']="View library";
+$strings['add_title']="Add library entry";
